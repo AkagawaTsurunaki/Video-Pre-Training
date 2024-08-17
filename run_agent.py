41963e0ebf3665811ca01f6094ca3b5f077f1a83
AkagawaTsurunaki <965145619@qq.com>
2024-08-17 16:49:15 +0800

    refactor: Remove the import statements of MineRL

--- run_agent.py
+++ run_agent.py
@@ -1,26 +1,22 @@
-from argparse import ArgumentParser
 import pickle
+from argparse import ArgumentParser
 
-from minerl.herobraine.env_specs.human_survival_specs import HumanSurvival
-
-from agent import MineRLAgent, ENV_KWARGS
-
-import controller
-from killer import listen_exit_key
 from pynput.keyboard import Key
 
+import controller
+from agent import MineRLAgent
+from killer import listen_exit_key
 from lib.screenshot import screenshot
 
 
 @listen_exit_key(Key.esc)
 def main(model, weights):
-    env = HumanSurvival(**ENV_KWARGS).make()
     print("---Loading model---")
     agent_parameters = pickle.load(open(model, "rb"))
     policy_kwargs = agent_parameters["model"]["args"]["net"]["args"]
     pi_head_kwargs = agent_parameters["model"]["args"]["pi_head_opts"]
     pi_head_kwargs["temperature"] = float(pi_head_kwargs["temperature"])
-    agent = MineRLAgent(env, policy_kwargs=policy_kwargs, pi_head_kwargs=pi_head_kwargs)
+    agent = MineRLAgent(policy_kwargs=policy_kwargs, pi_head_kwargs=pi_head_kwargs)
     agent.load_weights(weights)
 
     print("---Launching MineRL enviroment (be patient)---")
@@ -35,7 +31,6 @@ def main(model, weights):
         }
         minerl_action: dict = agent.get_action(agent_input)
         controller.minerl_action_to_env(minerl_action)
-
 
 
 if __name__ == "__main__":

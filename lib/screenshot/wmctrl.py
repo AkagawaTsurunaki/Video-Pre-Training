caa9663c38f6401c26a24cea64a9fd708f9458c5
AkagawaTsurunaki <AkagawaTsurunaki@outlook.com>
2024-08-17 16:54:22 +0800

    refactor: Remove the logger in activate_window

--- lib/screenshot/wmctrl.py
+++ lib/screenshot/wmctrl.py
@@ -1,10 +1,8 @@
 import subprocess
 
-from loguru import logger
-
 
 def activate_window(name: str):
-    logger.warning("You need to install wmctrl if it dose not work.")
+    # You need to install wmctrl if it dose not work.
     command = f'wmctrl -a "{name}"'
     result = subprocess.check_output(command, shell=True, executable="/bin/bash")
     result = result.decode('utf-8')

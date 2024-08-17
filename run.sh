d28430e82f32e152ec338771dc8d4e1ea7f906ab
AkagawaTsurunaki <AkagawaTsurunaki@outlook.com>
2024-08-17 15:06:32 +0800

    feat: shell 脚本

---
+++ run.sh
@@ -0,0 +1,3 @@
+source vpt
+python run_agent.py --model models/3x/foundation-model-3x.model --weights models/3x/foundation-model-3x.weights
+python run_agent.py --model models/2x/2x.model --weights models/2x/rl-from-foundation-2x.weights
